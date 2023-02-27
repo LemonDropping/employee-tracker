26599f3c7b55a70507ec2a7b01e451223ad29967
Cole Roberts <coleroberts76@gmail.com>
2023-02-27 11:31:00 -0600

    added employee table for database

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,13 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    manager_id INT,
+    role_id INT,
+    FOREIGN KEY (manager_id) REFERENCES role_id ON DELETE SET NULL,
+    FOREIGN KEY(role_id) REFERENCES employees (id) ON DELETE SET NULL
+);
